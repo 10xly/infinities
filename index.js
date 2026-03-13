cca3f2a4b5b45087a77066772bbcf80dd839a73f
tj-commits <thefoxjones@gmail.com>
2026-03-13 09:04:58 -0500

    refactor code to make ESLint pass

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-var */
+
 var min = require('math-intrinsics/min')
 var max = require('math-intrinsics/max')
 var noargs = require('noargs-wrapper')
 
 module.exports = {
-  positiveInfinity: noargs(min),
-  negativeInfinity: noargs(max)
+  negativeInfinity: noargs(max),
+  positiveInfinity: noargs(min)
 }
